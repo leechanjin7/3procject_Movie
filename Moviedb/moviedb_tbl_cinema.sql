3f61ddbd33ee89036bbb8084d23864dc5125f51e
mirdayo <mir6066@gmail.com>
2023-10-26 12:55:12 +0900

    reset aws+

---
+++ Moviedb/moviedb_tbl_cinema.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: moviedb
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_cinema`
+--
+
+DROP TABLE IF EXISTS `tbl_cinema`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_cinema` (
+  `cinemaId` int NOT NULL AUTO_INCREMENT,
+  `cinemaName` varchar(255) DEFAULT NULL,
+  `cinemaLocation` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`cinemaId`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_cinema`
+--
+
+LOCK TABLES `tbl_cinema` WRITE;
+/*!40000 ALTER TABLE `tbl_cinema` DISABLE KEYS */;
+INSERT INTO `tbl_cinema` VALUES (1,'신림','서울'),(2,'용산','서울'),(3,'홍대입구','서울'),(4,'경주','경북/대구'),(5,'동성로','경북/대구'),(6,'포항','경북/대구'),(7,'동해','강원'),(8,'춘천','강원'),(9,'화천','강원'),(10,'인천아시아드','경기/인천'),(11,'파주','경기/인천'),(12,'용인에버랜드','경기/인천');
+/*!40000 ALTER TABLE `tbl_cinema` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-26  9:17:23
---
+++ Moviedb/moviedb_tbl_member.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: moviedb
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_member`
+--
+
+DROP TABLE IF EXISTS `tbl_member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_member` (
+  `userId` varchar(255) NOT NULL,
+  `userPw` varchar(255) DEFAULT NULL,
+  `userName` varchar(255) DEFAULT NULL,
+  `userAddr1` varchar(255) DEFAULT NULL,
+  `userAddr2` varchar(255) DEFAULT NULL,
+  `userAddr3` varchar(255) DEFAULT NULL,
+  `userPhone` int DEFAULT NULL,
+  `userRole` int DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_member`
+--
+
+LOCK TABLES `tbl_member` WRITE;
+/*!40000 ALTER TABLE `tbl_member` DISABLE KEYS */;
+INSERT INTO `tbl_member` VALUES ('admin','$2a$10$Gy0wtMlMG/48wIswG4w/d.Jvx/GwxWiXBoKw3hNaIrHxcnkjp9KIe','관리자','48060','부산 해운대구 APEC로 17 (우동)','ffff',1000000000,1),('test','$2a$10$FFBxjm5mJ5niGYlG6ESuke.9Zkxvt/cWkX3K.AM.lSrKSZJO2LOyu','정정인데요','43109','대구 군위군 군위읍 개봉길 23-72','ㅇㅇ',1011112222,0),('test2','$2a$10$9rFUbikL6Z5zcQClpskzmOtO/awZ4WCslg/zv3/leZh5hp4SG9J6i','test2','03993','서울 마포구 연남로 1 (동교동)','dfadf',0,0);
+/*!40000 ALTER TABLE `tbl_member` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-26  9:17:23
